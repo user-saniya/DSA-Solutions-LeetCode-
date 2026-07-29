ebdb420b6eeeace2a91cadd4101696542da387d8
Saniya Akhtar <saniya87094@gmail.com>
2026-07-30 01:09:38 +0530

    Time: 2 ms (92.11%) | Memory: 47.5 MB (88.69%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,19 +1,28 @@
 class Solution {
     public void moveZeroes(int[] nums) {
    // Arrays.sort(nums);
-   int count=0;
-   int zero=0;
-   for(int i=0;i<nums.length;i++){
+//    int count=0;
+//    int zero=0;
+//    for(int i=0;i<nums.length;i++){
+//     if(nums[i]!=0){
+//         nums[count++]=nums[i];
+//     }
+//     else{
+//     zero++;
+//     }
+//     }
+//     while(zero>0){
+//         nums[count++]=0;
+//      zero--;
+//     }
+int count=0;
+for(int i=0;i<nums.length;i++){
     if(nums[i]!=0){
-        nums[count++]=nums[i];
-    }
-    else{
-    zero++;
-    }
-    }
-    while(zero>0){
-        nums[count++]=0;
-     zero--;
+        int swap=nums[i];
+        nums[i]=nums[count];
+        nums[count]=swap;
+        count++;
     }
     }
+  }
 }

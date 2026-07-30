a4011cf50b2ee6a18dbafa7130dd4c6257e4acb6
Saniya Akhtar <saniya87094@gmail.com>
2026-07-31 02:02:55 +0530

    Time: 3 ms (18.45%) | Memory: 47.9 MB (38.76%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -14,15 +14,19 @@ class Solution {
 //     while(zero>0){
 //         nums[count++]=0;
 //      zero--;
-//     }
-int count=0;
-for(int i=0;i<nums.length;i++){
-    if(nums[i]!=0){
-        int swap=nums[i];
-        nums[i]=nums[count];
-        nums[count]=swap;
-        count++;
+//    
+int i=0;
+int j=-1;
+while(i<nums.length){
+    if(nums[i]==0&&j==-1){
+        j=i;
     }
+    else if(nums[i]!=0&&j!=-1){
+        nums[j]=nums[i];
+        nums[i]=0;
+        j++;
     }
+    i++;
+}
   }
 }

9d2537817fb3c08dd972d2e0f00ba00d0d7b30f4
Saniya Akhtar <saniya87094@gmail.com>
2026-07-31 02:18:42 +0530

    Time: 1553 ms (5.00%) | Memory: 60.1 MB (13.85%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,25 +1,45 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>>result=new HashSet<>();
-       Arrays.sort(nums);
-       for(int i=0;i<nums.length-2;i++){
+    //     Set<List<Integer>>result=new HashSet<>();
+    //    Arrays.sort(nums);
+    //    for(int i=0;i<nums.length-2;i++){
+    //     int first=i+1;
+    //    int  last=nums.length-1;
+    //    while(first<last){
+    //     int sum=nums[i]+nums[first]+nums[last];
+    //     if(sum==0){
+    //         result.add(Arrays.asList(nums[i],nums[first],nums[last]));
+    //         first++;
+    //         last--;
+    //     }
+    //     else if(sum>0){
+    //     last--;
+    //     }
+    //     else{
+    //           first++;
+    //     }
+    //    }
+    //    }
+    //    return new ArrayList<>(result);
+    Set<List<Integer>>result=new HashSet<>();
+    Arrays.sort(nums);
+    for(int i=0;i<nums.length;i++){
         int first=i+1;
        int  last=nums.length-1;
-       while(first<last){
-        int sum=nums[i]+nums[first]+nums[last];
-        if(sum==0){
-            result.add(Arrays.asList(nums[i],nums[first],nums[last]));
-            first++;
-            last--;
-        }
-        else if(sum>0){
-        last--;
-        }
+        while(first<last){
+            int sum=nums[first]+nums[last]+nums[i];
+            if(sum==0){
+                result.add(Arrays.asList(nums[i],nums[first],nums[last]));
+              last--;
+            }
+           else if(sum>0){
+                last--;
+            }
         else{
-              first++;
+            first++;
         }
-       }
-       }
-       return new ArrayList<>(result);
+        }
+    }
+    return new ArrayList(result);
        }
     }

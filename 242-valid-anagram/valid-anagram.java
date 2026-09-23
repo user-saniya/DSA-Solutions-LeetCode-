ca9bebc7a4912b965b5d68fc60fb1e5c56af492a
Saniya Akhtar <saniya87094@gmail.com>
2026-09-23 08:11:24 +0530

    Time: 4 ms (83.76%) | Memory: 44 MB (97.51%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,19 +3,18 @@ class Solution {
         if (s.length() != t.length()) {
             return false;
         }
-        
-        int[] freq = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            freq[s.charAt(i) - 'a']++;
-            freq[t.charAt(i) - 'a']--;
+        int []count=new int[26];
+        for(int i=0;i<s.length();i++){
+            count[s.charAt(i)-'a']++;
+        }        
+        for(int i=0;i<t.length();i++){
+            count[t.charAt(i)-'a']--;
         }
-        
-        for (int i = 0; i < freq.length; i++) {
-            if (freq[i] != 0) {
+        for(int i=0;i<count.length;i++){
+            if(count[i]!=0){
                 return false;
             }
         }
-        
         return true;
     }
 }

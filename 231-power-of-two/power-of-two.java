b221227672281602e60de1863edac2883152454e
Saniya Akhtar <saniya87094@gmail.com>
2026-09-26 15:03:46 +0530

    Time: 1 ms (96.67%) | Memory: 42.5 MB (55.25%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,18 +1,5 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n==0){
-            return false;
-        }
-        int t=n;
-        while(n!=1){
-         if(n%2!=0){
-            return false;
-         }
-         else{
-            n=n/2;
-         }
-        
-        }
-        return true;
+     return n>0&&(n&(n-1))==0;
     }
 }
